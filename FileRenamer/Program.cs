df4fb7499448bca05036f7aac2cda67fb383985f
Kristoffer Kvamme <33118819+KristofferKvamme@users.noreply.github.com>
2019-09-27 10:22:19 +0200

    Upload

--- FileRenamer/Program.cs
+++ FileRenamer/Program.cs
@@ -1,7 +1,6 @@
 using System;
-using System.Collections.Generic;
+using System.IO;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace FileRenamer
@@ -10,12 +9,41 @@ namespace FileRenamer
     {
         static void Main(string[] args)
         {
-            // The code provided will print ‘Hello World’ to the console.
-            // Press Ctrl+F5 (or go to Debug > Start Without Debugging) to run your app.
-            Console.WriteLine("Hello World!");
-            Console.ReadKey();
+            while (true)
+            {
+                Console.Write("Folder Location: ");
+                var folderLoc = Console.ReadLine();
 
-            // Go to http://aka.ms/dotnet-get-started-console to continue learning how to build a console app! 
+                var d = new DirectoryInfo(folderLoc ?? throw new InvalidOperationException());
+                var fileArray = d.GetFiles();
+                Console.WriteLine(fileArray.Length);
+                try
+                {
+                    Parallel.ForEach(fileArray, (file) =>
+                    {
+                        Console.WriteLine(file.Name);
+                        var newName = "";
+                        var oldName = file.Name;
+                        var fluffRemoved = false;
+                        foreach (var character in oldName)
+                        {
+                            if (fluffRemoved) newName += character;
+                            else if (character == '_') fluffRemoved = true;
+                        }
+
+                        if (newName == "") return;
+                        Console.WriteLine($"Renamed: {oldName} to {newName}");
+                        File.Move(d.FullName + "\\" + oldName, d.FullName + "\\" + newName);
+                    });
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e);
+                }
+                Console.WriteLine("Renaming complete!");
+                Console.WriteLine("---------------------------------------------");
+                Console.WriteLine();
+            }
         }
     }
 }
